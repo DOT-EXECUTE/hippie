52b501012bd2824e9223651fb992fbe3111fd1c2
kav3n <dotexecute853@gmail.com>
2025-12-07 18:07:05 +1100

    Add web UI framework

---
+++ src/ui/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-row items-center font-semibold text-3xl">
+        <h1>hippie</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
